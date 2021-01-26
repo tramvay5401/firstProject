b28703ea42e117052c29529c1a172f1d174a31d1
tramvay5401 <tramvay5401@gmail.com>
2021-01-26 22:04:29 +0200

    create variable and qustion for user

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-alert(1)
+const numbersOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+	count: numbersOfFilms,
+	movies: {},
+	actors: {},
+	generas: [],
+	privat: false,
+};
+
+const firstQuestion = prompt('Один из последних просмотренных фильмов?', ''),
+	secondQuestion = prompt('На сколько его оцените?', ''),
+	theersQuestion = prompt('Один из последних просмотренных фильмов?', ''),
+	foursQuesrion = prompt('На сколько его оцените?', '');
+
+personalMovieDB.movies[firstQuestion] = secondQuestion;
+personalMovieDB.movies[theersQuestion] = foursQuesrion;
+
+console.log(personalMovieDB);
+
+		
